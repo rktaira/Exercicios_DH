2bcc6cfd17c7916273e2ec24007846379d3a64ac
rktaira <ricardoktaira@gmail.com>
2021-08-03 20:56:42 -0300

    adicionando exercícios de classes

---
+++ src/main/kotlin/classes/main.kt
@@ -0,0 +1,16 @@
+package classes
+
+fun main() {
+
+//exercício 1
+
+    var teste = Pessoa(50,"Homem", 30)
+
+    teste.aposentadoria()
+
+//exercício 2
+
+    var lista = Lista(1, 2, 3)
+
+    println(lista.multiplicar())
+}
