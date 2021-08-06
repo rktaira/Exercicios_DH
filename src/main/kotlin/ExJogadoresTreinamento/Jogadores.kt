6350fa99745e5db2601311169617e35289a7f891
rktaira <ricardoktaira@gmail.com>
2021-08-06 19:24:57 -0300

    Incluindo exercícios de criação de classes (conta, atleta, jogadores)

---
+++ src/main/kotlin/ExJogadoresTreinamento/Jogadores.kt
@@ -0,0 +1,30 @@
+package ExJogadoresTreinamento
+
+class JogadorDeFutebol (var nome: String, var energia: Int, var alegria: Int, var gols: Int, var experiencia: Int) {
+    fun fazerGol() {
+        energia = energia -5
+        alegria = alegria +5
+        gols = gols +1
+        println("GOOOOL!")
+    }
+
+    fun correr() {
+        if(energia>=10) {
+            energia = energia-10
+            println("$nome: Cansei")
+        }
+
+    }
+
+}
+
+class SessaoDeTreinamento (var experiencia: Int) {
+    fun treinarA (jogador: JogadorDeFutebol) {
+        jogador.correr()
+        jogador.fazerGol()
+        jogador.correr()
+        println("Experiência Inicial: "+jogador.experiencia)
+        jogador.experiencia=jogador.experiencia+ experiencia
+        println("Experiência Final: "+jogador.experiencia)
+    }
+}
---
+++ src/main/kotlin/ExercicioClientesContas/ExercicioConta.kt
@@ -0,0 +1,45 @@
+package ExercicioClientesContas
+
+/*
+Exercício 1: Clientes e Contas
+1. Definir uma nova classe Cliente contendo as seguintes propriedades:
+- nome
+- sobrenome
+2. Definir uma nova classe Conta contendo as seguintes propriedades:
+- número da conta
+- saldo
+- titular (o titular é um objeto do tipo Cliente).
+
+3. Criar uma função chamada depósito() que tenha como parâmetro de entrada quantia em
+dinheiro. Como estamos fazendo um depósito, a quantia de dinheiro é somada ao saldo.
+A função deve imprimir na tela o tipo de transação realizada e o novo saldo.
+
+4. Criar uma função chamada saque() que tenha como parâmetro de entrada quantia em
+dinheiro. Como estamos fazendo um saque, a quantia é subtraída do saldo. Caso o valor do
+saque a realizar seja maior que o saldo disponível, imprimir na tela: “Saldo insuficiente”.
+Caso contrário, a função deve imprimir na tela “Transação realizada!” e o novo saldo,
+concatenados.
+
+5. Definir um novo arquivo chamado ExercicioConta e criar uma função main(). Dentro da
+função main, criar dois Clientes, passando nome e sobrenome, e criar uma Conta para cada
+um, inicializando com o número da conta, saldo e titular. Em seguida, fazer um deposito()
+e um saque() em cada conta.
+
+ */
+
+fun main() {
+
+    var cliente1 = Cliente ("José", "Silva")
+    var conta1 = Conta (123, 1000, cliente1)
+
+    var cliente2 = Cliente ("Maria", "Silva")
+    var conta2 = Conta (321, 1000, cliente2)
+
+    println(conta1.depositar(1000))
+    println(conta1.sacar(500))
+
+    println()
+
+    println(conta2.depositar(2000))
+    println(conta2.sacar(700))
+}
